3527fe69ea9545308190d7dab3111172fdc77c3e
Naveed Shaikh <naveed.shaikh@gmail.com>
2019-01-10 20:47:08 +0000

    Incorporated changes from sudoku repo. Better error handling

--- app.py
+++ app.py
@@ -1,42 +1,41 @@
 from flask import abort
 from flask import Flask
-from flask import request,jsonify
-from sudoku import sudoku 
+from flask import request, jsonify
+from sudoku import sudoku
 
 app = Flask(__name__)
 
+
 def prepare_response(puzzle, solution, status='OK', message=None):
-    return {'puzzle':puzzle, 'solution':solution, \
-            'status':status, 'message':message}
+    return {'puzzle': puzzle, 'solution': solution,
+            'status': status, 'message': message}
+
 
 def solve_sudoku(puzzle):
     puzzle = puzzle.strip()
+    solution = None
 
-    if len(puzzle) != 81:
-        msg = 'Invalid sudoku. Input must be 81 chars ({0} provided)'.\
-                format(len(puzzle))
-        return prepare_response(puzzle, None, 'Error', msg) 
+    try:
+        solution = sudoku.solve(puzzle)
+    except sudoku.InvalidSudokuError, e:
+        return prepare_response(puzzle, None, 'error', str(e))
 
-    solution = sudoku.solve(puzzle)
+    if solution and sudoku.is_solved(solution):
+        return prepare_response(puzzle, solution)
 
-    if not solution:
-        return prepare_response(puzzle, None, 'error', 'Invalid sudoku provided')
+    return prepare_response(puzzle, None, 'error',
+                            'Unable to solve this sudoku. Notify developer!')
 
-    if solution and sudoku.is_solved(solution) \
-                    and sudoku.is_valid_solution(solution):
-        return prepare_response(puzzle, sudoku.grid_to_str(solution))
-  
-    return prepare_response(puzzle, None, 'error', 'Unable to solve this sudoku')
-        
+
 @app.route("/", methods=['POST'])
 def solve():
-    """ 
+    """
     Receive a HTTP POST request and unpack the payload (sudoku puzzles)
-    based on the header Content-Type. Content-Type can be either 
+    based on the header Content-Type. Content-Type can be either
     'application/x-www-form-urlencoded' (key-value pair in the HTTP form) or
     'application/json'. Key-value pair can only carry one sudoku puzzle whereas
     acceptable JSON format is a list of puzzle(s).
-    Return value is always a JSON in the following format: 
+    Return value is always a JSON in the following format:
     {
         data: [
             {
@@ -47,36 +46,35 @@ def solve():
             }
        ]
     }
-    
-    Raises HTTP 400 Bad Request if unacceptable Content-Type is provided. 
-    """ 
-    puzzles = [] 
+
+    Raises HTTP 400 Bad Request if unacceptable Content-Type is provided.
+    """
+    puzzles = []
 
     if request.content_type is None:
-        abort(400, 'Content-Type should be application/x-www-form-urlencoded '\
-                    'or application/json')
+        abort(400, 'Content-Type should be application/x-www-form-urlencoded '
+              'or application/json')
 
     if request.content_type == 'application/json':
         try:
             puzzles = request.json['sudoku']
-        except KeyError, e:
-            abort(400, 'Key sudoku missing in the input json. ' \
-                        'Expected format: {"sudoku":[list of 81-char-strings]}') 
-        
-        if len(puzzles) == 0:
-            abort(400, 'Missing list of sudoku puzzles. '\
-                        'Expected format: {"sudoku":[list of 81-char-strings]}')
+        except KeyError:
+            abort(400, 'Key sudoku missing in the input json. '
+                  'Expected format:{"sudoku":[list of 81-char-strings]}')
 
-    elif request.content_type  == 'application/x-www-form-urlencoded':
+        if len(puzzles) == 0:
+            abort(400, 'Missing list of sudoku puzzles. '
+                  'Expected format: {"sudoku":[list of 81-char-strings]}')
+
+    elif request.content_type == 'application/x-www-form-urlencoded':
         try:
             puzzles.append(request.form['sudoku'])
-        except KeyError, e:
-            abort(400, 'sudoku key missing in the input form. '\
-                        'Expected format: sudoku=<81-char-string>')
+        except KeyError:
+            abort(400, 'sudoku key missing in the input form. '
+                  'Expected format: sudoku=<81-char-string>')
     else:
-        abort(400, 'Content-Type should be application/x-www-form-urlencoded '\
-                    'or application/json')
-    
-    results = [solve_sudoku(p) for p in puzzles]
-    return jsonify({'data':results})
+        abort(400, 'Content-Type should be application/x-www-form-urlencoded '
+              'or application/json')
 
+    results = [solve_sudoku(p) for p in puzzles]
+    return jsonify({'data': results})
--- test_server.py
+++ test_server.py
@@ -5,17 +5,17 @@ from app import app
 
 Sudoku = namedtuple('Sudoku', ['puzzle', 'solution'])
 
+
 class SudokuAPITesT(unittest.TestCase):
 
     def setUp(self):
         app.config['TESTING'] = True
         self.sudoku = Sudoku(
-                '.1...8...3.472169...6....1....9.253..421.378..358.6....9....1...213874.9...5...2.',
-                '719638254354721698286495317678942531942153786135876942893264175521387469467519823')
+            '.1...8...3.472169...6....1....9.253..421.378..358.6....9....1...213874.9...5...2.',
+            '719638254354721698286495317678942531942153786135876942893264175521387469467519823')
         self.client = app.test_client()
 
-    
-    def assert_response(self, response, data_length, puzzles, solutions, \
+    def assert_response(self, response, data_length, puzzles, solutions,
                         status, messages):
         ''' Given a response, unpack the payload and test that client receives
             the right format and contents. Sudoku API will return a response
@@ -26,13 +26,13 @@ class SudokuAPITesT(unittest.TestCase):
                     {
                         'puzzle': <input>,
                         'solution': <solution if one is found>,
-                        'status_code': [ok|error], 
-                        'message': None if sudoku is solved else error message 
+                        'status_code': [ok|error],
+                        'message': None if sudoku is solved else error message
                 ]
             }
         '''
         response_data = json.loads(response.data)
-        
+
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response_data)
         self.assertTrue('data' in response_data)
@@ -47,83 +47,95 @@ class SudokuAPITesT(unittest.TestCase):
             else:
                 self.assertIn(messages[ndx], data['message'])
 
-   
     def test_missing_key_in_form(self):
         # Send an invalid key 'sudoku1' instead of 'sudoku'
-        response = self.client.post('/', data={'sudoku1':self.sudoku.puzzle})
+        response = self.client.post('/', data={'sudoku1': self.sudoku.puzzle})
         self.assertEqual(response.status_code, 400)
         self.assertIn('sudoku', response.data)
 
     def test_form_post(self):
-        response = self.client.post('/', data={'sudoku':self.sudoku.puzzle})
-        self.assert_response(response, 1, [self.sudoku.puzzle], 
-                            [self.sudoku.solution], ['ok'], [None])
-    
+        response = self.client.post('/', data={'sudoku': self.sudoku.puzzle})
+        self.assert_response(response, 1, [self.sudoku.puzzle],
+                             [self.sudoku.solution], ['ok'], [None])
+
     def test_invalid_len_in_form(self):
         # Append an extra char to our 81-chars input
         invalid_puzzle = self.sudoku.puzzle + '.'
-        response = self.client.post('/', data={'sudoku':invalid_puzzle})
-        self.assert_response(response, 1, [invalid_puzzle], [None], ['error'], \
-                            ['Invalid'])
+        response = self.client.post('/', data={'sudoku': invalid_puzzle})
+        self.assert_response(response, 1, [invalid_puzzle], [None], ['error'],
+                             ['Input length is 82'])
 
     def test_empty_puzzle_in_form(self):
-        response = self.client.post('/',data={'sudoku':''})
-        self.assert_response(response,1 , [''], [None], ['error'], ['Invalid'])
+        response = self.client.post('/', data={'sudoku': ''})
+        self.assert_response(response, 1, [''], [None], ['error'],
+                             ['Input length is 0'])
 
     def test_empty_post(self):
-       # No form data provided
-       response = self.client.post('/')
-       self.assertEqual(response.status_code, 400)
+        # No form data provided
+        response = self.client.post('/')
+        self.assertEqual(response.status_code, 400)
 
     def test_invalid_content_type(self):
-       response = self.client.post('/', content_type='blah')
-       self.assertEqual(response.status_code, 400)
+        response = self.client.post('/', content_type='blah')
+        self.assertEqual(response.status_code, 400)
 
     def test_json_post(self):
-        json_data = json.dumps({'sudoku':[self.sudoku.puzzle]})
-        response = self.client.post('/', data=json_data, content_type='application/json')
-        self.assert_response(response, 1, [self.sudoku.puzzle], [self.sudoku.solution], 
-                            ['ok'], [None])
+        json_data = json.dumps({'sudoku': [self.sudoku.puzzle]})
+        response = self.client.post('/', data=json_data,
+                                    content_type='application/json')
+        self.assert_response(response, 1, [self.sudoku.puzzle],
+                             [self.sudoku.solution], ['ok'], [None])
 
     def test_missing_key_in_json(self):
-        json_data = json.dumps({'sudoku1':[self.sudoku.puzzle]})
-        response = self.client.post('/', data=json_data, content_type='application/json')
+        json_data = json.dumps({'sudoku1': [self.sudoku.puzzle]})
+        response = self.client.post('/', data=json_data,
+                                    content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertIn('sudoku', response.data)
 
     def test_invalid_len_in_json(self):
         invalid_puzzle = self.sudoku.puzzle + '.'
-        json_data = json.dumps({'sudoku':[invalid_puzzle]})
-        response = self.client.post('/', data=json_data, content_type='application/json')
-        self.assert_response(response, 1, [invalid_puzzle], [None], ['error'], \
-                            ['Invalid'])
+        json_data = json.dumps({'sudoku': [invalid_puzzle]})
+        response = self.client.post('/', data=json_data,
+                                    content_type='application/json')
+        self.assert_response(response, 1, [invalid_puzzle], [None],
+                             ['error'], ['Input length is 82'])
 
     def test_missing_list_in_json(self):
-        json_data = json.dumps({'sudoku':''})
-        response = self.client.post('/', data=json_data, content_type='application/json')
+        json_data = json.dumps({'sudoku': ''})
+        response = self.client.post('/', data=json_data,
+                                    content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
     def test_empty_puzzle_in_json(self):
-        json_data = json.dumps({'sudoku':['']})
-        response = self.client.post('/', data=json_data, content_type='application/json')
-        self.assert_response(response, 1, [''], [None], ['error'], \
-                            ['Invalid'])
+        json_data = json.dumps({'sudoku': ['']})
+        response = self.client.post('/', data=json_data,
+                                    content_type='application/json')
+        self.assert_response(response, 1, [''], [None], ['error'],
+                             ['Input length is 0'])
 
     def test_multiple_puzzles_in_json(self):
-        json_data = json.dumps({'sudoku':[self.sudoku.puzzle, self.sudoku.puzzle]})
-        response = self.client.post('/', data=json_data, content_type='application/json')
-        self.assert_response(response, 2, [self.sudoku.puzzle, self.sudoku.puzzle], \
-                            [self.sudoku.solution, self.sudoku.solution], \
-                            ['ok', 'ok'], [None, None])
-        
+        json_data = json.dumps({'sudoku': [self.sudoku.puzzle,
+                                           self.sudoku.puzzle]})
+        response = self.client.post('/', data=json_data,
+                                    content_type='application/json')
+        self.assert_response(response, 2,
+                             [self.sudoku.puzzle, self.sudoku.puzzle],
+                             [self.sudoku.solution, self.sudoku.solution],
+                             ['ok', 'ok'], [None, None])
+
     def test_multiple_invalid_puzzles_in_json(self):
         invalid_puzzle = self.sudoku.puzzle + '.'
-        json_data = json.dumps({'sudoku':[self.sudoku.puzzle, invalid_puzzle]})
-        response = self.client.post('/', data=json_data, content_type='application/json')
-        self.assert_response(response, 2, [self.sudoku.puzzle, invalid_puzzle], \
-                            [self.sudoku.solution, None], \
-                            ['ok', 'error'], [None, 'Invalid'])
-       
+        json_data = json.dumps({'sudoku':
+                                [self.sudoku.puzzle, invalid_puzzle]})
+        response = self.client.post('/', data=json_data,
+                                    content_type='application/json')
+        self.assert_response(response, 2,
+                             [self.sudoku.puzzle, invalid_puzzle],
+                             [self.sudoku.solution, None],
+                             ['ok', 'error'],
+                             [None, 'Input length is 82'])
+
 
 if __name__ == '__main__':
     unittest.main()
